b91dfa76930a2d232ab302dc46111717125ef8c2
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-02 12:22:30 +0200

    problemi funzione

--- scriptjs/main.js
+++ scriptjs/main.js
@@ -49,8 +49,8 @@ function numeroRandom(min, max){
 
 
 
-//funzione includs
-// dato l'arrey e l'elemento da cercare, da true se l'elemento è presente altrimenti è falso 
+//FUNZIONI
+// INCLUDS
 function inArrey(arrey, elem){ 
     let cont = 0;
     let trovato = false;
@@ -63,18 +63,44 @@ function inArrey(arrey, elem){
     }
      return trovato; //return false
 }
-var bombe =[];
 
-while( bombe.length < 16  ){
-    var numeroGenerato = numeroRandom(1, 100)
-    if( !inArrey(bombe, numeroGenerato)){   //oppure bombe.includes(numeroGenerato)
-
-        bombe.push(numeroGenerato);
+// GENERA QUADRATI 
+function CreaQuadrato(numero){
+    for ( let i= 1; i <= numero; i++){
+        document.getElementById("campo").innerHTML+=`<div class="quadrato">${i}</div>`;
     }
+
 }
-console.log(bombe);
-// I numeri non possono essere duplicati.
+
+let btnGenera = document.getElementById("genera");
+
+btnGenera.addEventListener("click",
+function(){
+
+
+    var bombe =[];
+
+    // I numeri non possono essere duplicati.
+    while( bombe.length < 16  ){
+        var numeroGenerato = numeroRandom(1, 100)
+        if( !inArrey(bombe, numeroGenerato)){   //oppure bombe.includes(numeroGenerato)
+
+            bombe.push(numeroGenerato);
+        }
+    }
+    console.log(bombe);
+
+
+
+    //prendo i dati dal select per impostare il campo 
+    var livello = document.getElementById("livello").value
+    console.log(livello)
+
+    // CreaQuadrato()
+}
+
 // In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+
 // La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
 // Al termine della partita il software deve comunicare il punteggio.
 // BONUS: (da fare solo se funziona tutto il resto)
