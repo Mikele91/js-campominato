8b4a4a3c82edd486779ad8d8bcb85b64d6ff915d
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-06 10:25:41 +0200

    ultime modifiche

--- scriptjs/main.js
+++ scriptjs/main.js
@@ -1,53 +1,10 @@
-// vecchio programma 
-// function CreaQuadrato(numero){
-//     for ( let i= 1; i <= numero; i++){
-//         document.getElementById("campo").innerHTML+=`<div class="quadrato">${i}</div>`;
-//     }
 
-// }
-
-
-
-// // Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
-// let numeroUtente = parseInt(prompt("inserisci un numero multiplo di 10 ")) ;
-// while( ((numeroUtente % 10)!=0) ){
-//         numero =  parseInt(prompt(" ATTENZIONE inserisci un numero multiplo di 10 "));
-// }
-
-
-
-
-// CreaQuadrato(numeroUtente);
-
-
-// // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
-
-// document.getElementById("campo").addEventListener("click",
-// function(event){
-//     event.target.classList.toggle("color");
-//     // alert(event.target.innerHTML)
-//     console.log(event.target);
-// }
-// )
-
-
-
-//FINE VECCHIO PROGRAMMA
-
-
-// Ciao ragazzi,
-// Esercizio di oggi: Campo Minato
-// nome repo: js-campominato
-// Consegna
 // Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
-
 function numeroRandom(min, max){
 
         var random = Math.floor(Math.random() * (max-min +1)+min);
         return random;
 }
-
-
 // pulire i campi o refresh
 var btRefresh = document.getElementById("refresh");
 
@@ -95,6 +52,8 @@ function(){
                 alert("Hai vinto");
                 location.reload();
             }else{
+                document.getElementById("contenitore").classList.add("contenitore-perso");
+                document.getElementById("body").classList.add("perso");
                 alert("Hai perso")
                 location.reload();
             }
@@ -147,8 +106,6 @@ function(){
         )
         
     }
-
-    
     // I numeri non possono essere duplicati.
     while( bombe.length < 16  ){
         var numeroGenerato = numeroRandom(1, caselle);
@@ -162,12 +119,9 @@ function(){
 }
 );
 
-
-
 // In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
 var campo = document.getElementById("campo");
 var quadrato = document.getElementById("quadrato");
-
 var clickP =[];
 console.log(clickP);
 
@@ -192,17 +146,6 @@ function (event){
         console.log(caselle-16);
         console.log(" il punteggio ", clickP);
     }
-
-
-    // se è una casella già cliccata clickP.includes(i)
-    // altrimenti lo aggiungo all'array clickP
-    // clickP.length == possibilita hai vintoooooo
     );
 
-// La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
-// Al termine della partita il software deve comunicare il punteggio.
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
+
